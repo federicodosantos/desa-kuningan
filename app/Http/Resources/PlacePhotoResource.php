60277ca39cde56aacf67d963a90868029cc16543
Federico Roberto Dos Santos <128947498+federicodosantos@users.noreply.github.com>
2024-07-18 22:04:07 +0700

    refactor : add asset method in photo_path

--- app/Http/Resources/PlacePhotoResource.php
+++ app/Http/Resources/PlacePhotoResource.php
@@ -16,7 +16,7 @@ class PlacePhotoResource extends JsonResource
     {
         return [
             'id' => $this->id,
-            'photo_path' => 'storage/' . $this->photo_path,
+            'photo_path' => asset('storage/' . $this->photo_path),
             'created_at' => $this->created_at->toDateTimeString(),
             'updated_at' => $this->updated_at->toDateTimeString()
         ];
